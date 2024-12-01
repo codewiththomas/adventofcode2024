da5f6567597d8666479c4e50cc98f0d43cba3a9f
Thomas Bazan <thomas.bazan@gmail.com>
2024-12-01 21:30:03 +0100

    Solution Day01

---
+++ adventofcode2024/day01/Program.cs
@@ -0,0 +1,43 @@
+﻿//read input.txt file
+string[] lines = File.ReadAllLines("input.txt");
+
+var firstlist = new List<int>();
+var secondlist = new List<int>();
+
+foreach (string line in lines)
+{
+    var values = line.Split("   ");
+    int a = int.Parse(values[0]);
+    int b = int.Parse(values[1]);
+    firstlist.Add(a);
+    secondlist.Add(b);
+}
+
+var orderedFirstList = firstlist.OrderBy(x => x).ToArray();
+var orderedSecondList = secondlist.OrderBy(x => x).ToArray();
+
+
+int totalDistance = 0;
+
+for (int i = 0; i < orderedFirstList.Length; i++)
+{
+    int distance = orderedSecondList[i] - orderedFirstList[i];
+    totalDistance += Math.Abs(distance);
+}
+
+Console.WriteLine("Total distance: " + totalDistance.ToString());
+
+//SECOND PART
+int totalSimilarityScore = 0;
+
+for (int i = 0; i < orderedFirstList.Length; i++)
+{
+    int number = orderedFirstList[i];
+    int occurences = orderedSecondList.Count(x => x == number);
+    int similarityScore = occurences * number;
+    totalSimilarityScore += similarityScore;
+}
+
+Console.WriteLine("Total similarity score: " + totalSimilarityScore.ToString());
+
+
